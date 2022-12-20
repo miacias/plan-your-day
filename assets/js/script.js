6c0d7a4b7ae43d6d8619563776ca9a6ba8d23393
miaciasMAC <miaciasullo@gmail.com>
2022-12-20 18:47:12 -0500

    added save button variable created, beginning on click event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+    var saveBtn = $(".saveBtn");
+    saveBtn.click()
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
